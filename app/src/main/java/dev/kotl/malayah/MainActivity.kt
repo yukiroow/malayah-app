d565d8bd3afdb9d88eb78c660598e87ca1912f45
yukiroow <hcdominguez07@gmail.com>
2024-11-20 23:36:27 +0800

    Add Application Icons

--- app/src/main/java/dev/kotl/malayah/MainActivity.kt
+++ app/src/main/java/dev/kotl/malayah/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme(
-                darkTheme = true
+                darkTheme = false
             ) {
                 AppController()
             }
