3f2765cd2bfe71e9fba64d3c77de5560d1f3be82
yukiroow <hcdominguez07@gmail.com>
2024-11-20 20:30:13 +0800

    Remove flutter project in main branch

--- android/app/src/main/kotlin/com/example/malayah/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.malayah
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/const.dart
+++
@@ -1,1 +0,0 @@
-const API_KEY = "AIzaSyCNR5zDKw0zvbIUZMbrmJMhPpvktXFDLu0";
