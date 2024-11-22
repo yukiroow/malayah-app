936a38ef416bd60600615cbc53d2636bb53af35c
yukiroow <hcdominguez07@gmail.com>
2024-11-23 01:07:42 +0800

    Add Gemini Text Generation

--- app/src/main/java/dev/kotl/malayah/ui/ChatAppProper.kt
+++ app/src/main/java/dev/kotl/malayah/ui/ChatAppProper.kt
@@ -5,15 +5,20 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -23,9 +28,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -37,21 +45,36 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.generationConfig
 import dev.kotl.malayah.R
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChatAppProper(
     navController: NavController,
-    user: String
+    user: String,
+    model: ChatUiModel,
+    onSend: (String) -> Unit,
+    clear: () -> Unit,
 ) {
     var logoutIsOpen by remember { mutableStateOf(false) }
 
     if (logoutIsOpen) {
         LogoutDialog(
             onDismissRequest = { logoutIsOpen = false },
-            onConfirmation = { navController.navigate("landing") }
+            onConfirmation = {
+                clear()
+                navController.navigate("landing")
+            }
         )
     }
 
@@ -80,12 +103,48 @@ fun ChatAppProper(
                     }
                 }
             )
-        },
-        bottomBar = {
-
         }
     ) { innerPadding ->
-        ChatGreeting(user, innerPadding)
+        ConstraintLayout(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            val (messages, chatBox) = createRefs()
+
+            val listState = rememberLazyListState()
+
+            LaunchedEffect(model.messages.size) {
+                listState.animateScrollToItem(model.messages.size)
+            }
+            LazyColumn(
+                state = listState,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .constrainAs(messages) {
+                        top.linkTo(parent.top)
+                        bottom.linkTo(chatBox.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        height = Dimension.fillToConstraints
+                    },
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                items(model.messages) { item ->
+                    ChatBubble(item, user)
+                }
+            }
+            ChatInput(
+                onSend,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .constrainAs(chatBox) {
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+            )
+        }
     }
 }
 
@@ -125,7 +184,7 @@ fun ChatGreeting(user: String, innerPadding: PaddingValues) {
 }
 
 @Composable
-fun ChatBubble(message: Message, user: String) {
+fun ChatBubble(message: ChatUiModel.Message, user: String) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -142,10 +201,61 @@ fun ChatBubble(message: Message, user: String) {
                         bottomEnd = if (message.author == user) 0f else 48f
                     )
                 )
-                .background(MaterialTheme.colorScheme.primary)
+                .background(
+                    if (message.author == user)
+                        MaterialTheme.colorScheme.primaryContainer
+                    else
+                        MaterialTheme.colorScheme.secondaryContainer
+                )
                 .padding(16.dp)
         ) {
             Text(text = message.text)
+        }
+    }
+}
+
+@Composable
+fun ChatInput(
+    onSend: (String) -> Unit,
+    modifier: Modifier
+) {
+    var message by remember { mutableStateOf("") }
+    Row(modifier = modifier.padding(16.dp)) {
+        TextField(
+            value = message,
+            onValueChange = { newText ->
+                message = newText
+            },
+            modifier = Modifier
+                .weight(1f)
+                .padding(4.dp),
+            shape = RoundedCornerShape(24.dp),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+            placeholder = {
+                Text(text = "Share your thoughts")
+            }
+        )
+        IconButton(
+            onClick = {
+                val msg = message
+                if (msg.isBlank()) return@IconButton
+                onSend(message)
+                message = ""
+            },
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.Send,
+                contentDescription = "Send",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp)
+            )
         }
     }
 }
@@ -192,7 +302,51 @@ fun LogoutDialog(
     )
 }
 
-data class Message(
-    val text: String,
-    val author: String,
-)
+class ChatViewModel : ViewModel() {
+    val config = generationConfig { temperature = 0.7f }
+    val generativeModel = GenerativeModel(
+            modelName = "gemini-1.5-flash-latest",
+            apiKey = "",
+            generationConfig = config
+        )
+    private val chat = generativeModel.startChat(
+        history = listOf()
+    )
+    val conversation: StateFlow<List<ChatUiModel.Message>>
+        get() = _conversation
+    private val _conversation = MutableStateFlow(
+        listOf(ChatUiModel.Message("Greetings. I'm Malayah Bot!", "MalayahBot"))
+    )
+
+    fun clear() {
+        viewModelScope.launch {
+            _conversation.emit(listOf())
+        }
+    }
+
+    fun send(message: ChatUiModel.Message) {
+        viewModelScope.launch {
+            _conversation.emit(_conversation.value + message)
+
+            val response = chat.sendMessage("My name is ${message.author}. ${message.text}")
+            response.text?.let { modelResponse ->
+                _conversation.emit(
+                    _conversation.value + ChatUiModel.Message(
+                        modelResponse,
+                        "Malayah Bot"
+                    )
+                )
+            }
+        }
+    }
+}
+
+data class ChatUiModel(
+    val messages: List<Message>,
+    val addressee: String,
+) {
+    data class Message(
+        val text: String,
+        val author: String,
+    )
+}
